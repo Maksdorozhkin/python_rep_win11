27038efa912b4931a9e356ef05f00003f7aeaac2
md <maksdorozhkin@gmail.com>
2024-03-31 16:59:17 +0300

    чтение таблицы с помощью sqlite3

--- module_sqlite3/aqlite3.py
+++ module_sqlite3/aqlite3.py
@@ -4,11 +4,11 @@ DB_NAME = "sqlite_db.db"
 # with sqlite3.connect(DB_NAME) as sqlite_conn:
 #     print(sqlite_conn)
 #     print(sqlite3.version)
-courses = [
-    (351, "JavaScript course", 415, 100),
-    (614, "C++ course", 100, 23),
-    (721, "Java full course", 614, 400)
-]
+# courses = [
+#     (351, "JavaScript course", 415, 100),
+#     (614, "C++ course", 100, 23),
+#     (721, "Java full course", 614, 400)
+# ]
 
 # создаем подключение к базе данных
 # with sqlite3.connect(DB_NAME) as sqlite_conn:
@@ -16,11 +16,11 @@ courses = [
 #     sqlite_conn.execute(sql_request, (251, "Python course", 100, 30))
 #     sqlite_conn.commit()
 # создаем подключение с помощью цикла for
-with sqlite3.connect(DB_NAME) as sqlite_conn:
-    sql_request = "INSERT INTO courses VALUES(?, ?, ?, ?)"
-    for course in courses:
-        sqlite_conn.execute(sql_request, course)
-    sqlite_conn.commit()
+# with sqlite3.connect(DB_NAME) as sqlite_conn:
+#     sql_request = "INSERT INTO courses VALUES(?, ?, ?, ?)"
+#     for course in courses:
+#         sqlite_conn.execute(sql_request, course)
+#     sqlite_conn.commit()
 
 
 # создание новой таблицы
@@ -32,3 +32,12 @@ with sqlite3.connect(DB_NAME) as sqlite_conn:
 #     reviews_qty integer
 #     );"""
 #     sqlite_conn.execute(sql_request)
+
+# чтение таблицы с помощью sqlite3
+with sqlite3.connect(DB_NAME) as sqlite_conn:
+    sql_request = "SELECT * FROM courses WHERE reviews_qty>=30"
+    sql_cursor = sqlite_conn.execute(sql_request)
+    # for record in sql_cursor:
+    #     print(record[1])
+    records = sql_cursor.fetchall()
+    print(records)
