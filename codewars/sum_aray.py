9319513b521a39444908a37df80cec0080a098e0
md <maksdorozhkin@gmail.com>
2024-04-01 11:36:20 +0300

    Write a function that takes an array of numbers and returns the sum of the numbers. The numbers can be negative or non-integer.

---
+++ codewars/sum_aray.py
@@ -0,0 +1,7 @@
+# Write a function that takes an array of numbers and returns the sum of the numbers. The numbers can be negative or non-integer. If the array does not contain any numbers then you should return 0.
+
+def sum_array(a):
+    return sum(a)
+
+
+print(sum_array([1, 5.2, 4, 0, -1]))
