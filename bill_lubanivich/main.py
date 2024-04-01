c31062478865c6bc5c5068786a1df86c3e436920
md <maksdorozhkin@gmail.com>
2024-04-01 22:45:05 +0300

    while

--- bill_lubanivich/main.py
+++ bill_lubanivich/main.py
@@ -13,3 +13,44 @@ tweet_limit = 280
 tweet_string = "Blah" * 50
 if dif := tweet_limit - len(tweet_string) >= 0:
     print("Bla bla bla")
+
+# while
+
+count = 1
+while count <= 5:
+    print(count)
+    count += 1
+
+# # break
+# while True:
+#     stuff = input("String to capitalize [type q to quit]: ")
+#     if stuff == "q":
+#         break
+#     print(stuff.capitalize())
+
+
+# continue
+# while True:
+#     value = input("Integer, please [q to quit]: ")
+#     if value == 'q':  # выход
+#         break
+#     number = int(value)
+#     if not number % 2 == 0:  # нечетное число пропускаем
+#         continue
+#     print(number, "squared is", number*number)
+
+# прерываем цикл while с помощью else
+
+numbers = [1, 2, 3, 5]
+position = 0
+# Начинает цикл while, который продолжается, пока position меньше длины списка numbers
+while position < len(numbers):
+    # Получает значение на текущей позиции в списке numbers и присваивает его переменной number.
+    number = numbers[position]
+    if number % 2 == 0:  # четное
+        print('Found even number', number)
+        break
+    position += 1
+# Если цикл while завершается без встречи оператора break (т.е. ни одно чётное число не было найдено), выполняется этот блок else.
+else:
+    print("No even number found")
