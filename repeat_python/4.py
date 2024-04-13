9f4e8c047e2b1d80ac829eba4ef03b36be03dfb2
md <maksdorozhkin@gmail.com>
2024-04-13 20:54:07 +0300

    Читаю официальную документацию по python интерессное пробую гл.4.4 - 4.6

--- repeat_python/4.py
+++ repeat_python/4.py
@@ -58,4 +58,70 @@ print(range(10))
 
 print(sum(range(10)))
 
-# 4.4
+# 4.4 break continue
+# стратегия разделения на простые числа
+for n in range(2, 10):
+    for x in range(2, n):
+        if n % x == 0:
+            print(n, 'equals', x, '*', n//x)
+            break
+    else:
+        print(n, 'is a prime number')
+
+# стратегия разделения на четные и нечетные
+for num in range(2, 10):
+    if num % 2 == 0:
+        print("Found an even number", num)
+        continue
+    print("Found an odd number", num)
+
+# 4.6 match
+
+
+def http_error(status):
+    match status:
+        case 400:
+            return "Bad request"
+        case 404:
+            return "Not found"
+        case 418:
+            return "I`m a teapot"
+        case 401 | 403:
+            raise ValueError("Not a point")
+
+
+print(http_error(418))
+
+#
+point = (0, 20)
+match point:
+    case (0, 0):
+        print("Origin")
+    case (0, y):
+        print(f"Y={y}")
+    case (x, 0):
+        print(f"X={x}")
+    case (x, y):
+        print(f"X={x}, Y={y}")
+    case _:
+        raise ValueError("Not a point")
+
+
+class Point:
+    def __init__(self, x, y) -> None:
+        self.x = x
+        self.y = y
+
+
+def where_is(point):
+    match point:
+        case Point(x=0, y=0):
+            print("Origin")
+        case Point(x=0, y=y):
+            print(f"Y={y}")
+        case Point(x=x, y=0):
+            print(f"X={x}")
+        case Point():
+            print("Somewhere else")
+        case _:
+            raise ValueError("Not a point")
